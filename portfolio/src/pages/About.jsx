2b683e05907732ccd81faed03dce4e44ef474050
Caleb C <wolfertamer@gmail.com>
2025-05-13 19:40:33 -0700

    Improved About page and contact form

--- portfolio/src/pages/About.jsx
+++ portfolio/src/pages/About.jsx
@@ -7,21 +7,21 @@ function About() {
             <div className="row">
                 <div>
                     <h3>Background</h3>
-                    <p>I grew up in New Zealand and grew up in Santa Barbara. My dad is from fake Britain. </p>
+                    <p>First born in New Zealand, I moved to California when I was 5. Since middle school I've been learning how to program, starting with Java before moving on to more diverse languages. I eventually took up an internship at Novacoast where I worked on mobile applications and some web development.</p>
                 </div>
                 <div>
                     <h3>Education</h3>
-                    <p>I studied in the computer science academy at SBHS, then moved to Los Pueblos for my last year. I have associate's from SBCC, about to get Bachelor's from CSUF</p>
+                    <p>I studied in the Computer Science Academy at Santa Barabara High School before transfering to Dos Pueblos Senior High to graduate. I spent three years at Santa Barbara City College earning an Associate's Degree in Computer science before transfering to CalState Fullerton where I am set to graduate by the end of May.</p>
                 </div>
             </div>
             <div className="row">
                 <div>
                     <h3>Skills</h3>
-                    <p>HTML,CSS,JS,C,C++,Kotlin,Java,Swift,GitHub,XCode</p>
+                    <p>I can handle just about any language thrown at me, but my current specialty is in Web Development. Node.js is my framework of choice along with React for front end work. I also have used some Python/Flask and PHP for web. I also have extensive experience in both Kotlin and Swift for Android and iOS development respectively. I'm very familiar with GitHub and routinely help with issues regarding merge conflicts and lost work.</p>
                 </div>
                 <div>
                     <h3>Interests</h3>
-                    <p>D&D, Gaming, Programming, etc...</p>
+                    <p>I've recently been keeping up on the latest improvements in AI technology. Outside of programming I have an interest in Dungeons and Dragons and video editing.</p>
                 </div>
             </div>
             <a href="/CassinResume.pdf" download="CassinResume.pdf">
